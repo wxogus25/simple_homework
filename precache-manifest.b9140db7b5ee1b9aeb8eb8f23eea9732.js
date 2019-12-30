3429ea6ef14cb30a40c83e0a58eeeb37a1de7870
wxogus25 <wxogus25@gmail.com>
2019-12-30 16:07:58 +0900

    Updates

---
+++ precache-manifest.b9140db7b5ee1b9aeb8eb8f23eea9732.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "545133ea7704b4f0e4f729ba2a4c7efa",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "107fc58c806f9c3826a9",
+    "url": "/myapp/static/css/main.295984a9.chunk.css"
+  },
+  {
+    "revision": "2490ddb5ac73379fa9d9",
+    "url": "/myapp/static/js/2.e12614dd.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/myapp/static/js/2.e12614dd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "107fc58c806f9c3826a9",
+    "url": "/myapp/static/js/main.553d455a.chunk.js"
+  },
+  {
+    "revision": "aeb9cbf49147b5f4d6ff",
+    "url": "/myapp/static/js/runtime-main.cd228e50.js"
+  }
+]);
